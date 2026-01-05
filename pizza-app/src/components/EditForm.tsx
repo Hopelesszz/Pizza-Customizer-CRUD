01b527735e658d66e9f7622b29f4adad6ad450bf
Egor <savitsky.egor318@gmail.com>
2026-01-05 14:03:41 +0200

    Completed edit button with element editing

--- pizza-app/src/components/EditForm.tsx
+++ pizza-app/src/components/EditForm.tsx
@@ -1,8 +1,8 @@
-import AddPizzaButton from "../elements/AddPizzaButton";
+import EditButton from "../elements/EditButton";
 import { useForm, type SubmitHandler } from "react-hook-form";
 import {z} from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import type { PastryType, PizzaSize, Topping } from "../interfaces";
+import type { PastryType, PizzaSize, Topping,Pizza } from "../interfaces";
 import { usePizzaStore } from "../stores/pizzaStore";
 import { useNavigate } from 'react-router-dom';
 
@@ -14,11 +14,15 @@ const schema = z.object({
 });
 
 type FormData = z.infer<typeof schema> 
-const AddForm = () => {
+const EditForm: React.FC<{ pizzaToEdit: Pizza }> = ({ pizzaToEdit }) => {
     const navigate = useNavigate();
-    const pizzas = usePizzaStore((state) => state.pizzas);
-    const createPizza = usePizzaStore((state) => state.createPizza);
-    const addForm = useForm<FormData>({ resolver: zodResolver(schema) });
+    const updatePizza = usePizzaStore((state) => state.updatePizza);
+    const updateForm = useForm<FormData>({ defaultValues: {
+        name: pizzaToEdit.name,
+        pastry: pizzaToEdit.pastry.type,
+        size: pizzaToEdit.size.size,
+        ingredients: pizzaToEdit.ingredients.map(ingredient => ingredient.name)
+    },resolver: zodResolver(schema) });
     const onSubmit:SubmitHandler<FormData> = async(data) => {
         const size: PizzaSize = { size: data.size, price: data.size === 'small' ? 8 : data.size === 'medium' ? 10 : 12 };
         const pastry: PastryType = { type: data.pastry, price: data.pastry === 'thin' ? 2 : 2.5 };
@@ -77,151 +81,152 @@ const AddForm = () => {
                     break;
            }
         }
-        createPizza({
-            id: pizzas.length + 1,
+        const updatedPizza: Pizza = {
+            id: pizzaToEdit.id,
             name: data.name,
             size: size,
             pastry: pastry,
             live_price: size.price + pastry.price + ingredients_array.reduce((sum:number = 0, topping:Topping) => sum + topping.price, 0),
             ingredients: ingredients_array
-        }); 
+        };
+        updatePizza(updatedPizza);
         navigate('/');
     }
     return (
         <div className="content__container pt-[40px]">
             <h1 className='text-gray-700 flex w-[80vw] items-center font-[600] text-[34px]'>Add Pizza</h1>
             <div className='border-b border-gray-700 h-px'></div>
-            <form onSubmit={addForm.handleSubmit(onSubmit)} className="flex flex-col gap-[15px] w-[580px] border border-gray-700 rounded-[8px] p-[20px] mt-[13px]">
+            <form onSubmit={updateForm.handleSubmit(onSubmit)} className="flex flex-col gap-[15px] w-[580px] border border-gray-700 rounded-[8px] p-[20px] mt-[13px]">
                 <label htmlFor="name" className="text-gray-700 font-[500] text-[20px] w-[400px]">Name</label>
-                <input {...addForm.register("name", { required: "Name is required" })} type="text" name="name" placeholder=" Name" className="w-[400px] h-[40px] border border-gray-600 rounded-[8px] p-[10px] text-[18px]" />
-                {addForm.formState.errors.name && <span className="text-red-500">This field is required</span>}
+                <input {...updateForm.register("name", { required: "Name is required" })} type="text" name="name" placeholder=" Name" className="w-[400px] h-[40px] border border-gray-600 rounded-[8px] p-[10px] text-[18px]" />
+                {updateForm.formState.errors.name && <span className="text-red-500">This field is required</span>}
                 <div className="flex flex-col">
                     <h3 className="text-gray-700 font-[500] text-[20px] mb-2">Size</h3>
                     <div className="flex flex-col gap-2">
                         <label className="flex items-center gap-2 text-gray-700 cursor-pointer">
-                            <input type="radio" {...addForm.register("size")} value="small"/>
+                            <input type="radio" {...updateForm.register("size")} value="small"/>
                             Small
                         </label>
                         <label className="flex items-center gap-2 text-gray-700 cursor-pointer">
-                            <input type="radio" {...addForm.register("size")} value="medium"/>
+                            <input type="radio" {...updateForm.register("size")} value="medium"/>
                             Medium
                         </label>
                         <label className="flex items-center gap-2 text-gray-700 cursor-pointer">
-                            <input type="radio" {...addForm.register("size")} value="large"/>
+                            <input type="radio" {...updateForm.register("size")} value="large"/>
                             Large
                         </label>
                     </div>
                 </div>
-                {addForm.formState.errors.size && <span className="text-red-500">This option is required</span>}
+                {updateForm.formState.errors.size && <span className="text-red-500">This option is required</span>}
                 <div className="flex flex-col">
                     <h3 className="text-gray-700 font-[500] text-[20px] mb-2">Pastry</h3>
                     <div className="flex flex-col gap-2">
                         <label className="flex items-center gap-2 text-gray-700 cursor-pointer">
-                            <input type="radio" {...addForm.register("pastry")} value="thin"/>
+                            <input type="radio" {...updateForm.register("pastry")} value="thin"/>
                             Thin
                         </label>
                         <label className="flex items-center gap-2 text-gray-700 cursor-pointer">
-                            <input type="radio" {...addForm.register("pastry")} value="traditional"/>
+                            <input type="radio" {...updateForm.register("pastry")} value="traditional"/>
                             Traditional
                         </label>
                     </div>
                 </div>
-                {addForm.formState.errors.pastry && <span className="text-red-500">This option is required</span>}
+                {updateForm.formState.errors.pastry && <span className="text-red-500">This option is required</span>}
                 <span className="text-gray-700 font-[500] text-[20px] w-[400px]">Ingredients</span>
                 <div className="flex flex-row w-[530px] justify-between">
                     <div className="flex flex-col">
                         <h3 className="text-gray-700 font-semibold capitalize mb-2">Cheeses</h3>
                         <div className="flex flex-col gap-2">
                             <label className="flex items-center gap-2 text-gray-700 cursor-pointer">
-                                <input type="checkbox" value="mozzarella" {...addForm.register("ingredients")}/>
+                                <input type="checkbox" value="mozzarella" {...updateForm.register("ingredients")}/>
                                 Mozzarella
                             </label>
                             <label className="flex items-center gap-2 text-gray-700 cursor-pointer">
-                                <input type="checkbox" value="cheddar" {...addForm.register("ingredients")}/>
+                                <input type="checkbox" value="cheddar" {...updateForm.register("ingredients")}/>
                                 Cheddar
                             </label>
                             <label className="flex items-center gap-2 text-gray-700 cursor-pointer">
-                                <input type="checkbox" value="parmesan" {...addForm.register("ingredients")}/>
+                                <input type="checkbox" value="parmesan" {...updateForm.register("ingredients")}/>
                                 Parmesan
                             </label>
                             <label className="flex items-center gap-2 text-gray-700 cursor-pointer">
-                                <input type="checkbox" value="blue cheese" {...addForm.register("ingredients")}/>
+                                <input type="checkbox" value="blue cheese" {...updateForm.register("ingredients")}/>
                                 Blue Cheese
                             </label>
                         </div>
-                        {addForm.formState.errors.ingredients && <span className="text-red-500">This option is required</span>}
+                        {updateForm.formState.errors.ingredients && <span className="text-red-500">This option is required</span>}
                     </div>
                     <div className="flex flex-col">
                         <h3 className="text-gray-700 font-semibold capitalize mb-2">Meats</h3>
                         <div className="flex flex-col gap-2">
                             <label className="flex items-center gap-2 text-gray-700 cursor-pointer">
-                                <input type="checkbox" value="pepperoni" {...addForm.register("ingredients")}/>
+                                <input type="checkbox" value="pepperoni" {...updateForm.register("ingredients")}/>
                                 Pepperoni
                             </label>
                             <label className="flex items-center gap-2 text-gray-700 cursor-pointer">
-                                <input type="checkbox" value="bacon" {...addForm.register("ingredients")}/>
+                                <input type="checkbox" value="bacon" {...updateForm.register("ingredients")}/>
                                 Bacon
                             </label>
                             <label className="flex items-center gap-2 text-gray-700 cursor-pointer">
-                                <input type="checkbox" value="ham" {...addForm.register("ingredients")}/>
+                                <input type="checkbox" value="ham" {...updateForm.register("ingredients")}/>
                                 Ham
                             </label>
                             <label className="flex items-center gap-2 text-gray-700 cursor-pointer">
-                                <input type="checkbox" value="chicken" {...addForm.register("ingredients")}/>
+                                <input type="checkbox" value="chicken" {...updateForm.register("ingredients")}/>
                                 Chicken
                             </label>
                         </div>
-                        {addForm.formState.errors.ingredients && <span className="text-red-500">This option is required</span>}
+                        {updateForm.formState.errors.ingredients && <span className="text-red-500">This option is required</span>}
                     </div>
                     <div className="flex flex-col">
                         <h3 className="text-gray-700 font-semibold capitalize mb-2">Vegetables</h3>
                         <div className="flex flex-col gap-2">
                             <label className="flex items-center gap-2 text-gray-700 cursor-pointer">
-                                <input type="checkbox" value="tomato" {...addForm.register("ingredients")}/>
+                                <input type="checkbox" value="tomato" {...updateForm.register("ingredients")}/>
                                 Tomato
                             </label>
                             <label className="flex items-center gap-2 text-gray-700 cursor-pointer">
-                                <input type="checkbox" value="mushrooms" {...addForm.register("ingredients")}/>
+                                <input type="checkbox" value="mushrooms" {...updateForm.register("ingredients")}/>
                                 Mushrooms
                             </label>
                             <label className="flex items-center gap-2 text-gray-700 cursor-pointer">
-                                <input type="checkbox" value="olives" {...addForm.register("ingredients")}/>
+                                <input type="checkbox" value="olives" {...updateForm.register("ingredients")}/>
                                 Olives
                             </label>
                             <label className="flex items-center gap-2 text-gray-700 cursor-pointer">
-                                <input type="checkbox" value="bell pepper" {...addForm.register("ingredients")}/>
+                                <input type="checkbox" value="bell pepper" {...updateForm.register("ingredients")}/>
                                 Bell Pepper
                             </label>
                             <label className="flex items-center gap-2 text-gray-700 cursor-pointer">
-                                <input type="checkbox" value="onion" {...addForm.register("ingredients")}/>
+                                <input type="checkbox" value="onion" {...updateForm.register("ingredients")}/>
                                 Onion
                             </label>
                         </div>
-                        {addForm.formState.errors.ingredients && <span className="text-red-500">This option is required</span>}
+                        {updateForm.formState.errors.ingredients && <span className="text-red-500">This option is required</span>}
                     </div>
                     <div className="flex flex-col">
                         <h3 className="text-gray-700 font-semibold capitalize mb-2">Sauces</h3>
                         <div className="flex flex-col gap-2">
                             <label className="flex items-center gap-2 text-gray-700 cursor-pointer">
-                                <input type="checkbox" value="tomato sauce" {...addForm.register("ingredients")}/>
+                                <input type="checkbox" value="tomato sauce" {...updateForm.register("ingredients")}/>
                                 Tomato Sauce
                             </label>
                             <label className="flex items-center gap-2 text-gray-700 cursor-pointer">
-                                <input type="checkbox" value="bbq sauce" {...addForm.register("ingredients")}/>
+                                <input type="checkbox" value="bbq sauce" {...updateForm.register("ingredients")}/>
                                 BBQ Sauce
                             </label>
                             <label className="flex items-center gap-2 text-gray-700 cursor-pointer">
-                                <input type="checkbox" value="garlic sauce" {...addForm.register("ingredients")}/>
+                                <input type="checkbox" value="garlic sauce" {...updateForm.register("ingredients")}/>
                                 Garlic Sauce
                             </label>
                         </div>
-                        {addForm.formState.errors.ingredients && <span className="text-red-500">This option is required</span>}
+                        {updateForm.formState.errors.ingredients && <span className="text-red-500">This option is required</span>}
                     </div>
                 </div>
-                <AddPizzaButton disabled={addForm.formState.isSubmitting} click={addForm.handleSubmit(onSubmit)}/>
+                <EditButton disabled={updateForm.formState.isSubmitting} click={updateForm.handleSubmit(onSubmit)}/>
             </form>
         </div>
-  )
+    )
 }
 
-export default AddForm;
+export default EditForm;
--- pizza-app/src/pages/EditPizza.tsx
+++ pizza-app/src/pages/EditPizza.tsx
@@ -1,18 +1,18 @@
 import { useLocation } from "react-router-dom";
 import type { Pizza } from "../interfaces";
 import Header from '../components/Header';
+import EditForm from "../components/EditForm";
 
 const EditPizza = () => {
     const location = useLocation();
     const { pizzaToEdit } = location.state as { pizzaToEdit: Pizza };
-    console.log(pizzaToEdit);
 
     return (
         <>
             <Header/>
             <main className='main pb-[40px]'>
                 <div className='main__container'>
-
+                    <EditForm pizzaToEdit={pizzaToEdit} />
                 </div>
             </main>
         </>
