082b76f24dd8981a17d84fcc7f21f878289dcb24
Hopelesszz <hopelesssssz@gmail.com>
2026-01-05 16:23:21 +0200

    Starting work with darkmode

--- pizza-app/src/elements/ToggleIcon.tsx
+++ pizza-app/src/elements/ToggleIcon.tsx
@@ -1,9 +1,15 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMoon } from '@fortawesome/free-solid-svg-icons/faMoon';
+import { faSun } from '@fortawesome/free-solid-svg-icons/faSun';
+import { useThemeStore } from '../stores/themeStore';
 
 const ToggleIcon = () => {
+  const toggleDarkMode = useThemeStore((state) => state.toggleDarkMode);
   return (
-    <FontAwesomeIcon className='text-white size-[25px]' icon={faMoon} />
+    <>
+      <FontAwesomeIcon className='text-white size-[25px]' icon={faMoon} />
+      <FontAwesomeIcon className='text-white size-[25px]' icon={faSun} />
+    </>
   )
 }
 
